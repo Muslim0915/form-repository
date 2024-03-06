23c3e33dc5d82a4f64fef35ad21caacccaf79458
Muslim0915 <muslimpro0915@gmail.com>
2024-03-06 21:40:39 +0500

    some change

--- vite.config.mjs
+++ vite.config.mjs
@@ -12,7 +12,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/form-response/',
+  base: '/form-repository/',
   plugins: [
     Vue({
       template: { transformAssetUrls }
